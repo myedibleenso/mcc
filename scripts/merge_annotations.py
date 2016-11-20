25355f9663cb0b79dd1b39f00b0f29436382d347
Gus Hahn-Powell <gushahnpowell@gmail.com>
2016-11-20 01:02:43 -0700

    Updated script

--- scripts/merge_annotations.py
+++ scripts/merge_annotations.py
@@ -1,6 +1,7 @@
 import json
 import os
 from collections import Counter
+import argparse
 
 def get_annotations(d):
     data = []
@@ -16,10 +17,16 @@ def get_annotations(d):
 def filter_bugs(data):
     return [d for d in data if d["relation"] != "Bug"]
 
-directory = "/Users/gus/repos/reach-resources/recovered-annotations/annotation-splits/verified/"
-outfile = "/Users/gus/repos/reach-resources/wip/event-pairs.json"
 
-data = get_annotations(data)
+if __name__ == "__main__":
 
-# dump annotations
-json.dump(filter_bugs(data), open(outfile, "w"), indent=2, sort_keys=True)
+    parser = argparse.ArgumentParser(description='Merge json splits.')
+    parser.add_argument('--input', dest="input", type=str, required=True, help='a directory containing json files to be merged')
+    parser.add_argument('--output', dest='output', type=str, required=True, help='the destination directory for "event-pairs.json"')
+
+    args = parser.parse_args()
+    data = get_annotations(args.input)
+    outfile = "{}/event-pairs.json".format(args.output)
+
+    # dump annotations
+    json.dump(filter_bugs(data), open(outfile, "w"), indent=2, sort_keys=True)
